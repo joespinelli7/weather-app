33ef24ab6db8a5064715d63b33184712d370429c
github username <josephspinelli@yahoo.com>
2019-09-18 01:23:37 -0500

    rendering correct number of components and css styling

--- src/App.js
+++ src/App.js
@@ -21,9 +21,22 @@ class App extends React.Component {
   }
 
   render () {
+    const weatherArray = [];
+    for (let weather in this.state.days) {
+      weatherArray.push(this.state.days[weather]);
+    }
+    // console.log(weatherArray)
+
+    const daysArray = [];
+    for (let day in this.state.days) {
+      daysArray.push(day);
+    }
+    // console.log(daysArray)
     return (
       <div className="App">
-        <WeatherCard />
+        {daysArray.map(day => {
+          return <WeatherCard />
+        })}
       </div>
     );
   }
