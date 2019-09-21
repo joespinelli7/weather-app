ec63b2eece579e2db1d1507f324bf5d3c2e109b9
github username <josephspinelli@yahoo.com>
2019-09-21 00:58:35 -0500

    stuck rendering weather forecast and css changes

--- src/App.js
+++ src/App.js
@@ -5,37 +5,28 @@ import WeatherCard from './components/WeatherCard';
 class App extends React.Component {
   state={
     days: {
-      Mon: [78, 67],
-      Tue: [73, 69],
-      Wed: [79, 70],
-      Thu: [76, 67],
-      Fri: [80, 70]
-    },
-    forecast: {
-      Mon: "Sunny",
-      Tue: "Partly Cloudy",
-      Wed: "Sunny",
-      Thu: "Rain",
-      Fri: "Sunny"
+      Mon: {weather: [78, 67], forecast: "Sunny"},
+      Tue: {weather: [74, 65], forecast: "Partly Cloudy"},
+      Wed: {weather: [74, 65], forecast: "Sunny"},
+      Thu: {weather: [74, 65], forecast: "Rain"},
+      Fri: {weather: [80,70], forecast: "Sunny"}
     }
   }
 
   render () {
-    const weatherArray = [];
-    for (let weather in this.state.days) {
-      weatherArray.push(this.state.days[weather]);
-    }
-    // console.log(weatherArray)
-
     const daysArray = [];
+    const weatherArray = []
     for (let day in this.state.days) {
       daysArray.push(day);
     }
-    // console.log(daysArray)
+    console.log(this.state.days.Mon.weather)
     return (
       <div className="App">
         {daysArray.map(day => {
-          return <WeatherCard />
+          return <WeatherCard
+            key={day}
+            name={day}
+          />
         })}
       </div>
     );
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -3,11 +3,12 @@ import './WeatherCard.css';
 
 class WeatherCard extends React.Component {
   render() {
+      console.log(this.props)
     return (
       <div className={"WeatherCard"}>
-        <h2><b>Day of week</b></h2>
+        <h2><b>{this.props.name}.</b></h2>
         <p>image of forecast here</p>
-        <hr /> 
+        <hr />
         <p>forecast and degrees</p>
       </div>
     );
