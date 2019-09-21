d028065f5e64157273cf2c93ff21b131d57fc503
github username <josephspinelli@yahoo.com>
2019-09-20 23:59:12 -0500

    comments

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -7,7 +7,7 @@ class WeatherCard extends React.Component {
       <div className={"WeatherCard"}>
         <h2><b>Day of week</b></h2>
         <p>image of forecast here</p>
-        <hr />
+        <hr /> 
         <p>forecast and degrees</p>
       </div>
     );
