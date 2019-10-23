30bddf17c2153601969cfc6ba62c9b3a41f1d1a0
github username <josephspinelli@yahoo.com>
2019-10-22 22:55:35 -0500

    styles

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -6,7 +6,7 @@ class WeatherCard extends React.Component {
       console.log(this.props)
     return (
       <div className={"WeatherCard"}>
-        <h2><b>{this.props.name}.</b></h2>
+        <h2><b>{this.props.name}</b></h2>
         <p>image of forecast here</p>
         <hr />
         <p>forecast and degrees</p>
